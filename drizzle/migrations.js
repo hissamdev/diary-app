8ac09b7f13ddd9c2cecd45a4057870a3eaf76d71
Hissam <hissamdev@gmail.com>
2026-01-18 14:26:28 +0600

    Fixed migration issue

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_gigantic_bulldozer.sql';
 import m0001 from './0001_vengeful_the_hand.sql';
+import m0002 from './0002_glorious_hellcat.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
